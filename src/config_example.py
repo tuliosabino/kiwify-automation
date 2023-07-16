26c07b8e6cb0bec7e51d09520e95c611871b9436
Tulio <tulio.ax@hotmail.com>
2023-07-16 20:42:53 -0300

    chore: gitignore update

---
+++ src/config_example.py
@@ -0,0 +1,23 @@
+# ------------------------------ MAIN CONFIGS ---------------------------------
+# is displayed in the header of the site
+ACCOUNT_NAME = 'YOUR ACCOUNT NAME'
+
+DOMAIN = 'https://yourdomoian.com/'
+
+SUPPORT_EMAIL = 'youremail@yourprovider.com'
+PRODUCER_DISPLAY_NAME = 'Your Name that will be shown to your customers'
+
+# --------------------------- CHECKOUT CONFIGS --------------------------------
+
+
+# ----------------------- ORDERBUMP|UPSELL CONFIGS ----------------------------
+
+# name for a additional plan, leave empty to not create one
+ADDITIONAL_PLAN = 'OrderBump'
+
+# -----------------------------------------------------------------------------
+
+COURSES = {
+    'yourcourse': {'base_price': 47, 'additional': 27},
+    'yourcourse2': {'base_price': 47, 'additional': 27},
+}
